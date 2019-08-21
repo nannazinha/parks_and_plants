6f0c682a1b839851510df5e68c2cf860d18f49b2
Bruno Parga <brunoparga@gmail.com>
2019-08-21 18:03:05 -0300

    Allow creation of a Plant

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,24 @@
+class PlantsController < ApplicationController
+  def create
+    @garden = Garden.find(params[:garden_id])
+    # Alternative to lines 7 and 8:
+    # @plant = @garden.plants.build(plant_params)
+
+    @plant = Plant.new(plant_params)
+    @plant.garden = @garden
+    if @plant.save
+      # alternative: redirect_to @garden
+      redirect_to garden_path(@garden),
+                  notice: "Plant #{@plant.name} created successfully"
+    else
+      render 'garden/show'
+    end
+
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name, :image_url)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :gardens
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :gardens do
+    resources :plants, only: :create
+  end
 end
