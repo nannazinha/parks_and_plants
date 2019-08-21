c9464b6ed89fce769e2dbf02cec9b765c9c6d62b
Bruno Parga <brunoparga@gmail.com>
2019-08-21 17:36:25 -0300

    Add Plant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_21_202244) do
+ActiveRecord::Schema.define(version: 2019_08_21_203507) do
 
   create_table "gardens", force: :cascade do |t|
     t.string "name"
     t.string "banner_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "plants", force: :cascade do |t|
+    t.integer "garden_id"
+    t.string "name"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["garden_id"], name: "index_plants_on_garden_id"
   end
 
 end
